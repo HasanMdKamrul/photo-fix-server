e5fb05a6c4ce5b64efdb7134102089b6a1184c65
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-07 18:50:54 +0100

    Initial DB Connected

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ** Inports
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -16,7 +16,27 @@ app.get("/", (req, res) => {
   res.send(`Server is running on port ${port}`);
 });
 
+// ********** DB Collection **********
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ikallh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+console.log(uri);
+
+// ********** DB Collection **********
+
 // ** listener
 app.listen(port, () => {
+  client.connect((err) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("DB Connected");
+    }
+  });
   console.log(`Server is running on port: ${port}`);
 });
